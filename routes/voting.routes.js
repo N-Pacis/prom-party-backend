7412180f65d1b9742f75b33623d98eec145d1a3c
n-pacis <pacisnkubito@gmail.com>
2022-07-13 21:24:15 +0200

    Added voting

--- routes/voting.routes.js
+++ routes/voting.routes.js
@@ -1,11 +1,12 @@
 import express from 'express'
 import { createVote, getVotesByCoupleId, getVotesByCouples } from '../controllers/voting.controller.js';
+import { validateVoting } from '../validators/voting.validator.js';
 const router = express.Router()
 
 router.get("/votes/by-couples",getVotesByCouples)
 
 router.get("/votes/couples/:id", getVotesByCoupleId)
 
-router.post("/vote",createVote)
+router.post("/vote",validateVoting,createVote)
 
 export default router;

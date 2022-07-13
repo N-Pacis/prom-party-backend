bf79c2756e4815fd9ee0fb8afafb1f7e9e8b445d
n-pacis <pacisnkubito@gmail.com>
2022-07-13 21:22:39 +0200

    Added voting

---
+++ validators/voting.validator.js
@@ -0,0 +1,25 @@
+import Joi from 'joi';
+import _ from "lodash";
+
+export async function validateVoting(req, res, next) {
+    try {
+        const schema = Joi.object({
+            Names: Joi.string().required().label("Names"),
+            Email: Joi.string().email({ tlds: { allow: false } }).required().label("Email"),
+            coupleId: Joi.string().required().label("Couple Id")
+        })
+        
+        const { error } = schema.validate(req.body);
+        if (error) {
+            return res.status(400).json({
+                error: error.message,
+                message: "Unable to create the account."
+            })
+        }
+        return next()
+    }
+    catch (ex) {
+        return res.status(400).send(errorResponse(ex)
+)
+    }
+}
